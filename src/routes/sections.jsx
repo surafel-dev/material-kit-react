71359064dfeaa1295cec49fa48c0852bf5312d2c
surafel-dev <68854559+surafel-dev@users.noreply.github.com>
2024-01-23 12:57:08 +0300

    Update sections.jsx

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -4,6 +4,7 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
+export const StudentInfoPage = lazy(() => import ('src/pages/student-information'));
 export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
@@ -24,6 +25,7 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
+        { path:'studentInfo' element: <StudentInfoPage />},
         { path: 'user', element: <UserPage /> },
         { path: 'products', element: <ProductsPage /> },
         { path: 'blog', element: <BlogPage /> },
