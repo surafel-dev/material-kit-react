b19b7e7a13c944c67d466b5aa2010a6ed8824f8e
surafel-dev <68854559+surafel-dev@users.noreply.github.com>
2024-01-25 12:51:29 +0300

    Update nav.jsx

--- src/layouts/dashboard/nav.jsx
+++ src/layouts/dashboard/nav.jsx
@@ -240,9 +240,24 @@ function NavItem({ item }) {
                 <ListItemButton
                   component={RouterLink}
                   href={subItem.path}
+                  
                   sx={{
-                    pl: 4,
-                  }}
+                  minHeight: 44,
+        borderRadius: 0.75,
+        typography: 'body2',
+        color: 'text.secondary',
+        textTransform: 'capitalize',
+        fontWeight: 'fontWeightMedium',
+        ...(active && {
+          color: 'primary.main',
+          fontWeight: 'fontWeightSemiBold',
+          bgcolor: (theme) => alpha(theme.palette.primary.main, 0.08),
+          '&:hover': {
+            bgcolor: (theme) => alpha(theme.palette.primary.main, 0.16),
+          },
+        }),
+      }}
+                  
                 >
       <Box component="span" sx={{ width: 24,                     height: 24, mr: 2 }}>
         {subItem.icon}
