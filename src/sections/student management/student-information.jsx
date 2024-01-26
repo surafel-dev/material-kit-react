a189d13343fbda7d5bdad0ed15a7d5d1dfa2f051
surafel-dev <68854559+surafel-dev@users.noreply.github.com>
2024-01-26 13:47:16 +0300

    Update student-information.jsx

--- src/sections/student management/student-information.jsx
+++ src/sections/student management/student-information.jsx
@@ -10,24 +10,17 @@ import formDialog from 'src/layouts/dashboard/common/form-dialog';
 export default function studentInformation() {
 
 
-      const [openDialog, setOpenDialog] = useState(false);
-
+      
    return (
     
            <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Users</Typography>
 
-        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />} onClick={() => setOpenDialog(true)}>
+        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
           New student
         </Button>
       </Stack>
-      
-       <formDialog
-         openDialog = {openDialog}
-         setOpenDialog = {setOpenDialog}
-
-        />
 
         </Container>
 );
