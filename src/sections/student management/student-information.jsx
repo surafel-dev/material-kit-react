89b5139507274228cda09c95604139ab605e2b44
surafel-dev <68854559+surafel-dev@users.noreply.github.com>
2024-01-26 10:13:33 +0300

    Update student-information.jsx

--- src/sections/student management/student-information.jsx
+++ src/sections/student management/student-information.jsx
@@ -8,6 +8,15 @@ export default function studentInformation() {
 
    return (
     
+           <Container>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4">Users</Typography>
+
+        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+          New User
+        </Button>
+      </Stack>
+        </Container>
 
        <formDialog
          openDialog = {openDialog}
