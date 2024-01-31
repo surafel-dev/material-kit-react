1c439b3a9452f8b7835f3e0196ee3ce9324e0204
surafel-dev <68854559+surafel-dev@users.noreply.github.com>
2024-01-31 08:10:50 +0300

    Update student management

---
+++ src/sections/student management/student-info.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import Iconify from 'src/components/iconify';
+import formDialog from 'src/layouts/dashboard/common/form-dialog';
+
+export default function studentInformation() {
+
+
+
+   return (
+
+           <Container>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Typography variant="h4">Users</Typography>
+
+        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+          New student
+        </Button>
+      </Stack>
+
+        </Container>
+);
+
+};
